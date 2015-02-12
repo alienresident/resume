44d3e58458143f10d239a750a69aa4859b6b1677
Mark Reilly <mark@alienresident.net>
2015-02-11 21:16:02 -0500

    remove old font pairings

--- src/js/webfont.js
+++ src/js/webfont.js
@@ -1,7 +1,5 @@
 // Google Webfont
 WebFontConfig = {
-  //google: { families: [ 'Open+Sans:300italic,400italic,600italic,700italic,400,600,300,700:latin' ] }
-  //google: { families: [ 'Lato:400,700:latin', 'Merriweather:400,400italic,700,700italic:latin' ] }
   google: { families: [ 'Roboto:400,700,700italic,400italic:latin', 'Roboto+Slab::latin' ] }
 };
 (function() {
