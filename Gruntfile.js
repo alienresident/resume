f381e7f4be2879b73b3c666a573fd07ca45ef79b
Mark Reilly <mark@alienresident.net>
2018-03-03 19:02:15 -0500

    add .organization to uncss ignore options

--- Gruntfile.js
+++ Gruntfile.js
@@ -227,7 +227,7 @@ module.exports = function(grunt) {
 
     uncss: {
       options: {
-        ignore: ['.wf-roboto-i4-active i', '.wf-roboto-i4-active em', '.wf-roboto-n7-active b', '.wf-roboto-n7-active strong', '.wf-roboto-n4-active body', '.wf-robotoslab-n4-active h1', '.wf-robotoslab-n4-active h2', '.wf-robotoslab-n4-active h3', '.wf-robotoslab-n4-active h4', '.clone', '.down .clone', '.down .clone h1', '.down .clone .contact']
+        ignore: ['.wf-roboto-i4-active i', '.wf-roboto-i4-active em', '.wf-roboto-n7-active b', '.wf-roboto-n7-active strong', '.wf-roboto-n4-active body', '.wf-robotoslab-n4-active h1', '.wf-robotoslab-n4-active h2', '.wf-robotoslab-n4-active h3', '.wf-robotoslab-n4-active h4', '.clone', '.down .clone', '.down .clone h1', '.down .clone .contact', 'span.organization']
       },
       dist: {
         options: {
